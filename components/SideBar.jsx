57ffd866922fb2fca9d235beaf407d81b741beca
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-29 22:38:40 +0300

    Adding video and deletig png

---
+++ components/SideBar.jsx
@@ -0,0 +1,20 @@
+const SideBar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Hero</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Choice</li>
+            <li>Contact</li>
+        </ul>
+        <ul>
+            <li>X</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+        </ul>
+    </div>
+  )
+}
+
+export default SideBar

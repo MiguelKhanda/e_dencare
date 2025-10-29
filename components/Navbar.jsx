45677f4aab1e209bddc14f20c47ae7a4e863baff
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-30 00:29:46 +0300

    f

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,6 @@
+'use client'
+import {motion} from 'framer-motion'
+
 import Image from 'next/image'
 import { useState } from 'react'
 import {IoClose, IoMenu} from 'react-icons/io5'
@@ -7,6 +10,7 @@ const Navbar = () => {
   const handleClick=()=>{
     setIsClicked(!isClicked)
   }
+  const MotionIcon = motion.create(IoClose)
   return (
     <nav className='fixed flex w-full bg-primary items-center justify-between  px-4 py-4 backdrop-blur-md opacity-70 top-0 left-0 border-b-2 border-accent z-50'>
         <Image
@@ -19,17 +23,25 @@ const Navbar = () => {
         />
        
           {!isClicked ?(
-             <div className="menu p-2 bg-accent z-50 absolute right-10 sm:hidden">
+             <motion.div 
+             className="menu p-2 bg-accent z-50 absolute right-10 sm:hidden">
                 <IoMenu
                 className='text-3xl text-light opacity-65 z-50 sm:hidden' onClick={()=>handleClick()}
                 />
-              </div>
+              </motion.div>
             ):(
-              <div className="menu p-2 bg-primary z-50 absolute right-10 sm:hidden">
-                <IoClose 
-                  className='text-3xl' onClick={()=>handleClick()}
+              <motion.div 
+              initial={{x:100}}
+              animate={{x:0}}
+              transition={{duration:1,ease:'easeInOut'}}
+              className="menu p-2 z-50 absolute right-10 sm:hidden">
+                <MotionIcon
+                 initial={{x:100}}
+                 animate={{x:0}}
+                 transition={{duration:1,ease:'easeInOut'}}
+                className='text-3xl' onClick={()=>handleClick()}
                 />
-              </div>
+              </motion.div>
             )}            
         {isClicked&&(
           <SideBar/>
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -3,7 +3,13 @@ import { BiLogoFacebook, BiLogoInstagram, BiLogoTwitter } from 'react-icons/bi'
 
 const SideBar = () => {
   return (
-    <motion.div className="absolute right-0 top-0 bg-accent px-10 z-40 h-screen flex flex-col gap-5 text-secondary text-md">
+    <motion.div 
+        initial={{x:100}}
+        animate={{x:0}}
+        exit={{x:100}}
+        transition={{duration:1, ease:'easeInOut'}}
+        className="absolute right-0 top-0 bg-accent px-10 z-40 h-screen flex flex-col gap-5 text-secondary text-md">
+        
         <ul className='mt-40 flex flex-col gap-5'>
             <li>Hero</li>
             <li>About</li>

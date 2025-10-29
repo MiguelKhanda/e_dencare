a502d38499b33f39f1e7fd44f73d6e05b6c667f7
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-29 22:51:42 +0300

    gt

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
             muted
             playsInline
             onContextMenu={(e) => e.preventDefault()}
-            className='w-full h-screen object-cover opacity-20 z-10 absolute '
+            className='w-full h-screen object-cover opacity-40 z-10 absolute '
         >
             <source src = '/videos/Bg-Video.webm' type='video/webm'/>
         </video>

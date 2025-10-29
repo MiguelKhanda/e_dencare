5525361445195baa712e5cb18efe0b040f603fdb
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-29 23:01:10 +0300

    now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <nav className='fixed flex w-full bg-primary items-center justify-between  px-4 py-4 backdrop-blur-md opacity-70 top-0 left-0 border-b-2 border-light'>
         <Image
-            src='/images/logo.jpg'
+            src='/images/logo_cut.jpg'
             alt='EdenCare logo'
-            width={50}
-            height={50}
+            width={60}
+            height={70}
             priority
-            className='opacity-100 w-10'
+            className='opacity-100 w-20'
         />
         <div className="menu px-1 bg-accent">
             <IoMenu

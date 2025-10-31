b95d2c816452e00faf76c1cb8b59570f99aff3e4
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-31 07:41:57 +0300

    second

--- components/About.jsx
+++ components/About.jsx
@@ -8,10 +8,10 @@ const About = () => {
        src='images/about-bg.svg'
        width={40}
        height={40}
-       className="w-full  mt-6 -z-10 absolute opacity-30 h-[80vh]"
+       className="w-full  mt-6 -z-10 absolute opacity-50 h-[80vh]"
       />
       <h1 className="text-primary text-2xl text-center mt-8">About Us</h1>
-      <div className="text-accent w-[90%] mx-auto mt-8 tracking-wide mb-20">
+      <div className="text-dark w-[90%] mx-auto mt-8 tracking-wide mb-20">
         <p>
           EdenCare Consultants is a premier consulting firm that delivers comprehensive and innovative solutions
           across multiple sectors.

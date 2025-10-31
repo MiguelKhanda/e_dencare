ccbb9cab2ca840a2bb3fc857a9dfc98a910410ba
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-31 08:20:39 +0300

    h

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,11 @@
 'use client'
+import './Navbar.module.css'
 import {motion} from 'framer-motion'
-
 import Image from 'next/image'
 import { useState } from 'react'
 import {IoClose, IoMenu} from 'react-icons/io5'
 import SideBar from './SideBar'
+import { BiLogoFacebook, BiLogoInstagram, BiLogoTwitter } from 'react-icons/bi'
 const Navbar = () => {
   const [isClicked, setIsClicked] = useState(false)
   const handleClick=()=>{
@@ -21,7 +22,18 @@ const Navbar = () => {
             priority
             className='opacity-100 w-20'
         />
-       
+        <ul className='gap-5 items-center justify-center hidden text-xl sm:flex'>
+          <li>Hero</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Choice</li>
+          <li>Contact</li>
+        </ul>
+        <ul className='gap-2  hidden sm:flex'>
+          <BiLogoFacebook size={25} className='hover:text-secondary'/>
+          <BiLogoInstagram size={25} className='hover:text-secondary'/>
+          <BiLogoTwitter size={25} className='hover:text-secondary'/>
+        </ul>
           {!isClicked ?(
              <motion.div 
              className="menu p-2 bg-accent z-50 absolute right-10 sm:hidden">

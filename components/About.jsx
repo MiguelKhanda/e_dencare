26fff1e50ac9014b571dd0bd69cfc3b2a6fc8031
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-31 07:38:07 +0300

    Morning first

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,32 @@
+import Image from "next/image"
+
+
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Image 
+       src='images/about-bg.svg'
+       width={40}
+       height={40}
+       className="w-full  mt-6 -z-10 absolute opacity-30 h-[80vh]"
+      />
+      <h1 className="text-primary text-2xl text-center mt-8">About Us</h1>
+      <div className="text-accent w-[90%] mx-auto mt-8 tracking-wide mb-20">
+        <p>
+          EdenCare Consultants is a premier consulting firm that delivers comprehensive and innovative solutions
+          across multiple sectors.
+        </p>
+        <p>
+          Our team comprises seasoned experts with extensive experience in research,
+          policy analysis, community engagement, environmental assessments, soil and water conservation and
+          sustainable development.
+        </p>
+        <p>
+          We strive to empower communities, influence policy, and promote sustainable
+          practices through our tailored services.
+        </p>
+      </div>
+    </div>
   )
 }
 

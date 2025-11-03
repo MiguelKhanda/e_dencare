e290cefad621b21936bbf75cd88566028f462087
MiguelKhanda <miguelkhanda84@gmail.com>
2025-11-03 21:06:58 +0300

    f

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,10 +5,14 @@ import { useForm } from "react-hook-form"
 
 const Contact = () => {
   const {register, handleSubmit, formState:{errors}} = useForm()
+  const onSubmit = (data) => {
+    console.log(data);
+    
+  }
   return (
     <div className="mt-100 bg-dark h-screen">
       <h1 className="text-primary pt-6 text-center">Contact Us</h1>
-      <form className="bg-primary flex flex-col w-[80%] mx-auto rounded-2xl p-5 mt-30 shadow-secondary shadow-2xl">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-primary flex flex-col w-[80%] mx-auto rounded-2xl p-5 mt-30 shadow-secondary shadow-2xl">
         <div className="flex flex-col">
           <label htmlFor="">Name</label>
           <input 
@@ -19,8 +23,29 @@ const Contact = () => {
         </div>
           <div className="flex flex-col">
           <label htmlFor="">Email</label>
-          <input type="text" placeholder="Enter your name"/>
+          <input 
+          type="text" 
+          className="outline-light border-light border-2 rounded bg-accent text-secondary"
+          {...register('email', {
+            required: 'Email is required', 
+            pattern:{
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: 'Invalid email address',
+            },
+          })}
+          />
+          
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
+        <div>
+          <textarea className="bg-accent text-light w-full mt-6 rounded p-6 h-30"
+          {...register('message', {
+            required:'Message is required',
+            minLength:{value:10, message:'Minimum 10 characters'}
+          })}></textarea>
+          {errors.message && <p>{errors.message.message}</p>}
+        </div>
+        <button type="submit"  className="bg-accent rounded p-2 w-[40%] mx-auto text-light mt-4">Submit</button>
       </form>
     </div>
   )

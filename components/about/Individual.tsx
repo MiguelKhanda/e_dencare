16ea2fc590e9f5c876f6524c0d2293cc4f570d25
MiguelKhanda <miguelkhanda84@gmail.com>
2025-11-14 11:45:39 +0300

    Button  commit

--- components/about/Individual.tsx
+++ components/about/Individual.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image.js'
 const Individual = () => {
   return (
     <div className='flex flex-col w-[95%] mx-auto'>
-        <h1 className='text-primary text-center pb-10'>Meet the team</h1>
+        <h1 className='text-primary text-center pb-8'>Meet the team</h1>
         {data.map((data)=>(
             <div className='flex mb-20' key={data.id}>
-                <div className='w-1/2 text-secondary text-xs'>
+                <div className='w-1/2 bg-linear-to-r  from-secondary to-primary text-transparent bg-clip-text text-xs'>
                     <h1 className='text-center bg-linear-to-r from-primary to-dark'>{data.name}</h1>
                     <p>Age: {data.age}</p>
                     <p>Education: <br/>{data.education}</p>

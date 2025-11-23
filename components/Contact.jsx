2e758f15ffb33c8ed3bccf8723f4a1f2424eaf79
MiguelKhanda <miguelkhanda84@gmail.com>
2025-11-23 18:14:52 +0300

    Changing stuff second

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,23 +1,39 @@
 'use client'
-
+import emailjs from 'emailjs-com'
 import { useForm } from "react-hook-form"
-import Alert from './Alert'
-
+import {useState, useRef} from 'react'
 const Contact = () => {
-  const {register, handleSubmit, formState:{errors}} = useForm()
+  const {register, handleSubmit, formState:{errors}, reset} = useForm()
+  const formRef = useRef()
+  const [status, setStatus] = useState(null)
   const onSubmit = (data) => {
-    <Alert/>
-    console.log(data);
+   emailjs.sendForm(
+    'service_l6udqpa',
+    'template_se3rrgw',
+    formRef.current,
+    'xLcvI02xJ2CCF_-F0'
+   ).then(
+    (result) => {
+      console.log(result.text);
+      setStatus({type:'success',message:'Message sent successfully'})
+      reset()
+    },
+    (error) => {
+      console.log(error.text)
+      setStatus({type:'error', message:'Failed to send message'})
+    }
+   )
     
   }
   return (
     <section id ='contact' className="bg-dark flex flex-col sm:pb-40 pb-40">
       <hr className="w-full text-primary shadow-2xl shadow-secondary"/>
       <h1 className="text-primary pt-6 text-center text-2xl">Contact Us</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-primary flex flex-col w-[90%] m-auto rounded-2xl p-5 mt-30 shadow-secondary shadow-2xl sm:w-[70%] sm:py-12 md:w-[50%] md:p-10">
+      <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className="bg-primary flex flex-col w-[90%] m-auto rounded-2xl p-5 mt-30 shadow-secondary shadow-2xl sm:w-[70%] sm:py-12 md:w-[50%] md:p-10">
         <div className="flex flex-col">
           <label htmlFor="">Name</label>
           <input 
+          name='name'
           placeholder="Jean Christophe"
           className="px-2 outline-light border-light border-2 rounded bg-accent text-secondary"
           {...register('name', {required:'Name is required'})}
@@ -27,6 +43,7 @@ const Contact = () => {
           <div className="flex flex-col">
           <label htmlFor="">Email</label>
           <input 
+          name='email'
           placeholder="jeanchriskas@gmail.com"
           type="text" 
           className="px-2 outline-light border-light border-2 rounded bg-accent text-secondary"
@@ -43,7 +60,7 @@ const Contact = () => {
         </div>
         <div>
           <label>Message</label>
-          <textarea placeholder="Write to us" className="bg-accent w-full mt-1 rounded p-2 h-30 md:h-50 text-secondary"
+          <textarea name='message' placeholder="Write to us" className="bg-accent w-full mt-1 rounded p-2 h-30 md:h-50 text-secondary"
           {...register('message', {
             required:'Message is required',
             minLength:{value:10, message:'Minimum 10 characters'}

37d4cfa7b547346a5e13524c075c9c54bf367e4e
MiguelKhanda <miguelkhanda84@gmail.com>
2025-11-23 17:50:32 +0300

    Changing stuff second

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     
   }
   return (
-    <section id ='contact' className="bg-dark flex flex-col sm:pb-40">
+    <section id ='contact' className="bg-dark flex flex-col sm:pb-40 pb-40">
       <hr className="w-full text-primary shadow-2xl shadow-secondary"/>
       <h1 className="text-primary pt-6 text-center text-2xl">Contact Us</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="bg-primary flex flex-col w-[90%] m-auto rounded-2xl p-5 mt-30 shadow-secondary shadow-2xl sm:w-[70%] sm:py-12 md:w-[50%] md:p-10">

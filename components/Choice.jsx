ef6acefb0bb3b1a984bacaf132adbcb76b3a3ee3
MiguelKhanda <miguelkhanda84@gmail.com>
2025-11-28 12:16:31 +0300

    f

--- components/Choice.jsx
+++ components/Choice.jsx
@@ -4,6 +4,7 @@ import OneSlider from './OneSlider'
 const Choice = () => {
   return (
     <section id='choice' className="h-screen bg-dark text-primary">
+      <hr className='w-screen border-primary'/>
       <h1 className="text-xl text-center pt-3">Why Choose Us</h1>
       <OneSlider data ={choice_Data} color = 'primary'/>
     </section>
